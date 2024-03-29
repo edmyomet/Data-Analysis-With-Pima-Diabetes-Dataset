e37dd50ecd83fa6bda2370afedf8ddcbc3db888b
edmyomet <shrut828@gmail.com>
2024-03-29 18:02:42 +0530

    Path Change
    File path change and addition of few more statistical metrics

--- src/__eda.py
+++ src/__eda.py
@@ -8,30 +8,30 @@ import scipy.stats as stats
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 
-
+sns.set_palette(palette='deep')
 
 class Diabetes:
     def __init__(self):
-        self.df = pd.read_csv(r'C:\Users\shrut\OneDrive\Pictures\wallpapers\Documents\Projects\Data-Analysis-With-Pima-Diabetes-Dataset\datasets\diabetes.csv')
+        self.df = pd.read_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\datasets\diabetes.csv')
         self.feature_list = self.df.columns
         
     def __dataset_description(self):
-        self.df.describe().to_csv(r'C:\Users\shrut\OneDrive\Pictures\wallpapers\Documents\Projects\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\dataset_description.csv')
+        self.df.describe().to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\dataset_description.csv')
 
     def __freq(self):
         for feature in self.feature_list:
-            pd.DataFrame(self.df[feature].value_counts()).to_csv(r'C:\Users\shrut\OneDrive\Pictures\wallpapers\Documents\Projects\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\freq.csv',mode='a')
+            pd.DataFrame(self.df[feature].value_counts()).to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\freq.csv',mode='a')
             
     def __mean(self):
         self.mean = self.df.mean(numeric_only=True,skipna=True)
-        self.mean.to_csv(r'C:\Users\shrut\OneDrive\Pictures\wallpapers\Documents\Projects\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\mean.csv')
+        self.mean.to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\mean.csv')
     def __median(self):
         self.median = self.df.median(numeric_only=True,skipna=True)
-        self.median.to_csv(r'C:\Users\shrut\OneDrive\Pictures\wallpapers\Documents\Projects\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\median.csv')
+        self.median.to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\median.csv')
         
     def __mode(self):
         self.mode = self.df.mode(numeric_only=True)
-        self.mode.to_csv(r'C:\Users\shrut\OneDrive\Pictures\wallpapers\Documents\Projects\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\mode.csv')
+        self.mode.to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\mode.csv')
         
     def __mean_median_deviation(self):
         temp = pd.DataFrame( (np.array(self.mean) - np.array(self.median)) / 100)
@@ -50,16 +50,18 @@ class Diabetes:
             self.df.fillna(self.median, inplace=True)
     
     def __boxplot(self):
-        self.hue = np.random.randint(100,size=8)
         fig,axes= plt.subplots(1,8,figsize=(25,4),squeeze=False)
         fig.suptitle('Box Plot For Outlier Detection')
         for i in range(8):
             sns.cubehelix_palette(as_cmap=True)
             sns.boxplot(self.df[self.feature_list[i]],width=0.2,ax=axes[0][i])
             axes[0][i].set_title(f'Box Plot for {self.feature_list[i]}')
-        plt.savefig(r'C:\Users\shrut\OneDrive\Pictures\wallpapers\Documents\Projects\Data-Analysis-With-Pima-Diabetes-Dataset\output\img\boxplot.png')
+        plt.savefig(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\img\boxplot.png')
     
-    
+    def __std(self):
+        self.std = self.df.std(numeric_only=True)
+        self.std.to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\std.csv')
+        
     
     def __normal_distr(self):
         fig,axes = plt.subplots(1,8,figsize=(25,4),squeeze=False)
@@ -67,14 +69,39 @@ class Diabetes:
         for i in range(8):
             sns.histplot(data=self.df,x=self.df[self.feature_list[i]],kde=True,ax=axes[0][i])
             axes[0][i].set_title(f'{self.feature_list[i]}')
-        plt.savefig(r'C:\Users\shrut\OneDrive\Pictures\wallpapers\Documents\Projects\Data-Analysis-With-Pima-Diabetes-Dataset\output\img\histplot.png')
+        plt.savefig(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\img\histplot.png')
         
-    def __skew(self):
-        pass
+    def __skewness(self):
+        self.skew = self.df.skew(numeric_only=True)
+        self.skew.to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\skew.csv')
     
-    def __kurt(self):
-        pass
+    def __kurtosis(self):    
+        self.kurt = self.df.kurtosis(numeric_only=True)
+        self.kurt.to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\kurtosis.csv')
+    
+    def __outlier_remove(self):
+        for feature in self.feature_list:
+            q1 = self.df[feature].quantile(0.25)
+            q3 = self.df[feature].quantile(0.75)
             
+            iqr = q3 - q1
+            
+            upper = q3 + 1.5 * iqr
+            lower = q1 - 1.5 * iqr
+            
+            self.df_cleaned = self.df[(self.df[feature] < upper ) & (self.df[feature] > lower)]
+            self.df = self.df_cleaned
+                       
+    def __cov(self):
+        self.cov = self.df.cov(numeric_only=True)
+        self.cov.to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\cov.csv')
+    
+    def __corr(self):
+        self.corr = self.df.corr(numeric_only=True)
+        self.corr.to_csv(r'.\Data-Analysis-With-Pima-Diabetes-Dataset\output\results\corr.csv')
+    
+    def __scatter(self):
+        pass
         
     def analysis(self):
         self.__dataset_description()
@@ -84,6 +111,11 @@ class Diabetes:
         self.__mode()
         self.__fill_nulls()
         self.__boxplot()
+        self.__skewness()
+        self.__kurtosis()
+        self.__normal_distr()
+        self.__outlier_remove()
+        
 
         
 
